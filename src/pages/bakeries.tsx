54407e90ecd571f671d3a4390f176779295e2a93
maltmann <michael.altmann@gmail.com>
2023-11-16 09:46:40 -0600

    Add geolocation

--- src/pages/bakeries.tsx
+++ src/pages/bakeries.tsx
@@ -134,10 +134,22 @@ export default function Bakeries() {
       dragPan: false,
     });
     map.current.on("load", () => generateLayer());
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      })
+    );
     return () => {
       map.current?.off("load", generateLayer);
     };
   });
+
   useEffect(() => {
     generateTripPath();
   }, [generateTripPath]);
